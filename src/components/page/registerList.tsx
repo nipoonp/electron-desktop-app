86f9be21224319d3a4d849ed47355713ff855176
nipoonp <patel_nipoon@yahoo.co.nz>
2021-06-02 17:00:21 +1200

    Disconnect register on restaurant change

--- src/components/page/registerList.tsx
+++ src/components/page/registerList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Space6 } from "../../tabin/components/spaces";
 import { toast } from "../../tabin/components/toast";
 import { useRegister } from "../../context/register-context";
@@ -8,36 +8,45 @@ import { Title2Font } from "../../tabin/components/fonts";
 import { useHistory } from "react-router-dom";
 import { beginOrderPath } from "../main";
 import { useRestaurant } from "../../context/restaurant-context";
+import { FullScreenSpinner } from "../../tabin/components/fullScreenSpinner";
 
 export const RegisterList = () => {
     const history = useHistory();
     const { restaurant } = useRestaurant();
     const { register, connectRegister, disconnectRegister } = useRegister();
+    const [showFullScreenSpinner, setShowFullScreenSpinner] = useState(false);
 
     if (!restaurant) return <div>This user has not selected any restaurant.</div>;
 
     const onConnect = async (key: string) => {
         try {
+            setShowFullScreenSpinner(true);
+
             await connectRegister(key);
 
+            setShowFullScreenSpinner(false);
             history.replace(beginOrderPath);
-            toast.success("Register connected successfully set.");
         } catch (e) {
+            setShowFullScreenSpinner(false);
             toast.error(e);
         }
     };
 
     const onDisconnect = async (key: string) => {
         try {
+            setShowFullScreenSpinner(true);
+
             await disconnectRegister(key);
-            toast.success("Register disconnected successfully set.");
         } catch (e) {
             toast.error(e);
+        } finally {
+            setShowFullScreenSpinner(false);
         }
     };
 
     return (
         <>
+            {showFullScreenSpinner && <FullScreenSpinner show={true} />}
             <div
                 style={{
                     display: "flex",
--- src/components/page/restaurantList.tsx
+++ src/components/page/restaurantList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Space6 } from "../../tabin/components/spaces";
 import { Separator4 } from "../../tabin/components/separator";
 import { ButtonV2 } from "../../tabin/components/buttonv2";
@@ -7,16 +7,36 @@ import { useHistory } from "react-router-dom";
 import { useRestaurant } from "../../context/restaurant-context";
 import { FullScreenSpinner } from "../../tabin/components/fullScreenSpinner";
 import { beginOrderPath } from "../main";
+import { useRegister } from "../../context/register-context";
+import { toast } from "../../tabin/components/toast";
 
 export const RestaurantList = () => {
     const history = useHistory();
-    const { selectRestaurant, userRestaurants } = useRestaurant();
+    const { restaurant, selectRestaurant, userRestaurants } = useRestaurant();
+    const { register, disconnectRegister } = useRegister();
     const storedSelectedRestaurantId = localStorage.getItem("selectedRestaurantId");
+    const [showFullScreenSpinner, setShowFullScreenSpinner] = useState(false);
 
-    if (!userRestaurants) return <FullScreenSpinner show={true} text={"Loading user"} />;
+    useEffect(() => {
+        if (userRestaurants && userRestaurants.length == 1) {
+            onConnect(userRestaurants[0].id);
+        }
+    }, [userRestaurants]);
 
-    const onDisconnect = () => {
-        selectRestaurant(null);
+    const onDisconnect = async () => {
+        try {
+            setShowFullScreenSpinner(true);
+
+            if (register) {
+                await disconnectRegister(register.id);
+            }
+
+            selectRestaurant(null);
+        } catch (e) {
+            toast.error(e);
+        } finally {
+            setShowFullScreenSpinner(false);
+        }
     };
 
     const onConnect = async (restaurantId: string) => {
@@ -24,11 +44,10 @@ export const RestaurantList = () => {
         history.push(beginOrderPath);
     };
 
-    if (userRestaurants.length == 1) {
-        onConnect(userRestaurants[0].id);
-    }
     return (
         <>
+            {!userRestaurants && <FullScreenSpinner show={true} text={"Loading user"} />}
+            {showFullScreenSpinner && <FullScreenSpinner show={true} />}
             <div
                 style={{
                     display: "flex",
@@ -40,35 +59,37 @@ export const RestaurantList = () => {
                 <div style={{ width: "500px" }}>
                     <Title2Font>Select a restaurant to access</Title2Font>
                     <Space6 />
-                    {userRestaurants.map((restaurant, index) => (
-                        <>
-                            {index != 0 && <Separator4 />}
-                            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                                <div>{restaurant.name}</div>
-                                {storedSelectedRestaurantId == restaurant.id ? (
-                                    <>
-                                        <ButtonV2
-                                            onClick={() => {
-                                                onDisconnect();
-                                            }}
-                                        >
-                                            {"Disconnect"}
-                                        </ButtonV2>
-                                    </>
-                                ) : (
-                                    <>
-                                        <ButtonV2
-                                            onClick={() => {
-                                                onConnect(restaurant.id);
-                                            }}
-                                        >
-                                            {"Use"}
-                                        </ButtonV2>
-                                    </>
-                                )}
-                            </div>
-                        </>
-                    ))}
+                    {userRestaurants &&
+                        userRestaurants.map((userRestaurant, index) => (
+                            <>
+                                {index != 0 && <Separator4 />}
+                                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                                    <div>{userRestaurant.name}</div>
+                                    {storedSelectedRestaurantId == userRestaurant.id ? (
+                                        <>
+                                            <ButtonV2
+                                                onClick={() => {
+                                                    onDisconnect();
+                                                }}
+                                            >
+                                                {"Disconnect"}
+                                            </ButtonV2>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <ButtonV2
+                                                onClick={() => {
+                                                    onConnect(userRestaurant.id);
+                                                }}
+                                                disabled={restaurant ? true : false}
+                                            >
+                                                {"Use"}
+                                            </ButtonV2>
+                                        </>
+                                    )}
+                                </div>
+                            </>
+                        ))}
                 </div>
             </div>
         </>
