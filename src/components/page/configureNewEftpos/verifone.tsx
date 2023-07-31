8ce9ef4cb8d59fdcdc24f78a17105946c3d3991e
Nipoon Patel <patel_nipoon@yahoo.co.nz>
2023-07-31 21:59:04 +1200

    Update verifone

--- src/components/page/configureNewEftpos/verifone.tsx
+++ src/components/page/configureNewEftpos/verifone.tsx
@@ -18,7 +18,7 @@ export const Verifone = () => {
         setShowSpinner(true);
 
         try {
-            const res: IEftposTransactionOutcome = await createTransaction(amount, ipAddress, portNumber, "TEST-CONFIGURE");
+            const res: IEftposTransactionOutcome = await createTransaction(amount, ipAddress, portNumber, "TEST-CONFIGURE", (message: string) => {});
 
             alert(res.message);
         } catch (errorMessage) {
