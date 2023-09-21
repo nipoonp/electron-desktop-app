2dbfee46afd9685dc9ccd472817706826f7ce9c0
Nipoon Patel <patel_nipoon@yahoo.co.nz>
2023-09-22 11:58:42 +1200

    Update table number

--- src/components/page/tableNumber.tsx
+++ src/components/page/tableNumber.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router";
 import { checkoutPath } from "../main";
 import { useCart } from "../../context/cart-context";
@@ -6,39 +6,108 @@ import { PageWrapper } from "../../tabin/components/pageWrapper";
 import { Button } from "../../tabin/components/button";
 import { Input } from "../../tabin/components/input";
 import { useRestaurant } from "../../context/restaurant-context";
+import KioskBoard from "kioskboard";
+import { FiX } from "react-icons/fi";
 
 import "./tableNumber.scss";
 
 export default () => {
     const navigate = useNavigate();
-    const { tableNumber, setTableNumber } = useCart();
-    const [table, setTable] = useState(tableNumber);
     const { restaurant } = useRestaurant();
+    const { tableNumber, setTableNumber } = useCart();
+
+    const numpadRef = useRef(null);
+
+    // const [table, setTable] = useState(tableNumber);
+    const [tableError, setTableError] = useState(false);
+
+    useEffect(() => {
+        if (numpadRef.current) {
+            KioskBoard.run(numpadRef.current, {
+                theme: "light",
+                keysArrayOfObjects: [
+                    {
+                        "0": "7",
+                        "1": "8",
+                        "2": "9",
+                    },
+                    {
+                        "0": "4",
+                        "1": "5",
+                        "2": "6",
+                    },
+                    {
+                        "0": "1",
+                        "1": "2",
+                        "2": "3",
+                    },
+                    {
+                        "0": "0",
+                        "1": ".",
+                    },
+                ],
+            });
+        }
+    }, [numpadRef]);
 
     if (restaurant == null) throw "Restaurant is invalid!";
 
+    const onClose = () => {
+        navigate(`${checkoutPath}`);
+    };
+
     const onNext = () => {
-        if (tableNumber) {
+        //@ts-ignore
+        const table = numpadRef.current.value;
+
+        if (table) {
             setTableNumber(table);
             navigate(`${checkoutPath}`);
         } else {
-            setTableNumber(table);
-            navigate(`${checkoutPath}`);
+            setTableError(true);
         }
     };
 
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setTable(e.target.value);
-    };
+    // const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     setTable(e.target.value);
+    //     setTableError(false);
+    // };
 
     return (
         <>
             <PageWrapper>
                 <div className="table-number">
-                    <div className="h2 mb-12">Enter the table number you wish to dine on (click next if you are unsure)</div>
+                    <div className="close-button-wrapper">
+                        <FiX className="close-button" size={36} onClick={onClose} />
+                    </div>
+                    <div className="h2 mb-6">Enter your table number</div>
+                    <div className="mb-6 table-image-container">
+                        <img
+                            alt="Table Image"
+                            className="table-image"
+                            src="https://tabin-public.s3.ap-southeast-2.amazonaws.com/images/table-flag.webp"
+                        />
+                        <div className="table-image-override"></div>
+                    </div>
                     <div className="mb-12" style={{ width: "300px" }}>
                         <div className="h3 mb-2">Table Number</div>
-                        <Input type="number" autoFocus={true} onChange={onChange} value={table ?? ""} />
+                        {/* <Input
+                            type="number"
+                            autoFocus={true}
+                            onChange={onChange}
+                            value={table ? table.slice(0, 2) : ""}
+                            error={tableError ? "Required" : ""}
+                        /> */}
+                        <input
+                            className={`inputFromKey input`}
+                            ref={numpadRef}
+                            data-kioskboard-type="numpad"
+                            type="number"
+                            // autoFocus={true}
+                            // onChange={onChange}
+                            // value={table ? table.slice(0, 2) : ""}
+                        />
+                        {tableError && <div className="text-error mt-2">{tableError ? "Required" : ""}</div>}
                     </div>
                     <Button onClick={onNext}>Next</Button>
                 </div>
