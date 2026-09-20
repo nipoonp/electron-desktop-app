9e30aedcce4e66471a2b32f2f5a0ff1fea3c0e64
Nipoon Patel <patel_nipoon@yahoo.co.nz>
2026-09-20 12:36:05 +1200

    Update image load

--- src/tabin/components/cachedImage.tsx
+++ src/tabin/components/cachedImage.tsx
@@ -1,23 +1,32 @@
 import { useEffect, useState } from "react";
 import { useRestaurant } from "../../context/restaurant-context";
 import { toDataURL } from "../../util/util";
+import { useElectron } from "../../context/electron-context";
 
 import "./cachedImage.scss";
 
 export const CachedImage = (props: IProps) => {
+    const { checkParentView } = useElectron();
+    const { electron, reactNativeWebView } = checkParentView();
+    const isBrowser = !electron && !reactNativeWebView;
+
     const { restaurantProductImages } = useRestaurant();
     const [imageSrc, setImageSrc] = useState(restaurantProductImages[props.url]);
 
     useEffect(() => {
+        if (isBrowser) return;
+
         if (!imageSrc) {
             toDataURL(props.url, (dataUrl) => {
                 restaurantProductImages[props.url] = dataUrl;
                 setImageSrc(dataUrl);
             });
         }
-    }, [props.url]);
+    }, [props.url, isBrowser]);
 
-    return imageSrc ? (
+    return isBrowser ? (
+        <img className={props.className} style={props.style} src={props.url} alt={props.alt} />
+    ) : imageSrc ? (
         <img className={props.className} style={props.style} src={imageSrc} alt={props.alt} />
     ) : (
         <div className={`placeholder-item ${props.className}`}></div>
